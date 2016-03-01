9aa1c2c8d49d9566e479fb566a78c8a3168bfe5f
Dan B. Fujita <dan.b.fujita@gmail.com>
2016-03-01 15:00:49 -0500

    convert responce to json

--- authentication.js
+++ authentication.js
@@ -1,63 +1,68 @@
 var request = require('request');
 var config ={
-loginURL :"",
-signURL :"",
+  loginURL :"",
+  signURL :"",
 
 }
-
 
 function login(username, password,callback) {
   var callBackData = {};
   request({
-  url: config.loginURL,
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({"usrinfo":{"username": username}, "password":  password})
-}, function(error, response, body){
-  if(error) {
-    callBackData.statusCode = -1
-  } else {
-    if(response.statusCode!=201){
-      callBackData.statusCode = response.statusCode
-      callback(callBackData)
+    url: config.loginURL,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({"usrinfo":{"username": username}, "password":  password})
+  }, function(error, response, body){
+    if(error) {
+      callBackData.statusCode = -1
+    } else {
+      var jsonBody = JSON.parse(body);
+      if(response.statusCode!=201){
+        callBackData.statusCode = jsonBody.statusCode
+        callback(callBackData)
 
-    }else{
-      callBackData.statusCode = response.statusCode
-      callBackData.token = body.token
-      callback(callBackData)
+      }else{
+        callBackData.statusCode = jsonBody.statusCode
+        callBackData.token = jsonBody.body.token
+        callback(callBackData)
+      }
     }
-  }
-});
+  });
 }
 
 function signup(usrinfo,password,callback) {
   var callBackData = {};
   request({
-  url: config.signURL,
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({"usrinfo":usrinfo, "password":  password})
-}, function(error, response, body){
-  if(error) {
-    callBackData.statusCode = -1
-  } else {
-    if(response.statusCode!=201){
-      callBackData.statusCode = response.statusCode
+    url: config.signURL,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({"usrinfo":usrinfo, "password":  password})
+  }, function(error, response, body){
+    if(error) {
+      callBackData.statusCode = -1
       callback(callBackData)
 
-    }else{
-      callBackData.statusCode = response.statusCode
-      callBackData.token = body.token
-      callback(callBackData)
+    } else {
+      var jsonBody = JSON.parse(body);
+      if(jsonBody.statusCode!=201){
+        callBackData.statusCode = jsonBody.statusCode
+        callback(callBackData)
+
+      }else{
+        callBackData.statusCode = jsonBody.statusCode
+        callBackData.token = jsonBody.body.token
+        callback(callBackData)
+
+      }
     }
-  }
-});
+  });
 }
 
 module.exports = {
-  login: login
+  login: login,
+  signup: signup
 }
